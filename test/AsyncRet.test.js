347b3c31bb37c17e95a37fcb00444bf964f36108
Maicon Diniz Filippsen <filippsen@universe.ai>
2020-12-08 18:16:05 +0100

    Add AsyncRet tests
    + Add 3 new tests covering Exception
    
    + Add 3 new tests covering SocketError
    
    + Add 7 new tests covering fromProps
    
    + Add 3 new tests covering Timeout
    
    + Add 3 new tests covering Busy
    
    - Remove test covering AsyncRet.Error

--- test/AsyncRet.test.js
+++ test/AsyncRet.test.js
@@ -64,11 +64,186 @@ describe("AsyncRet", () => {
         });
     });
 
-    describe("Error", () => {
+    describe("Exception", () => {
+        test("input is undefined", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.Exception();
+                assert(data.isError());
+                assert(data.isException());
+                assert(!data.errorMessage());
+                assert(!data.props._ret);
+                assert(!data.props._error.msg);
+            });
+        });
+
+        test("input is null", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.Exception(null);
+                assert(data.isError());
+                assert(data.isException());
+                assert(!data.props._ret);
+                assert(data.props._error.msg == null);
+            });
+        });
+
         test("input is string", () => {
             assert.doesNotThrow(() => {
-                const data = AsyncRet.Error("it is an error");
-                assert(data.message == "it is an error");
+                const data = AsyncRet.Exception("it is an exception");
+                assert(data.isError());
+                assert(data.isException());
+                assert(!data.props._ret);
+                assert(data.props._error.msg == "it is an exception");
+            });
+        });
+    });
+
+    describe("SocketError", () => {
+        test("input is undefined", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.SocketError();
+                assert(data.isSocketError());
+                assert(!data.props._ret);
+                assert(!data.props._error.msg);
+            });
+        });
+
+        test("input is null", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.SocketError(null);
+                assert(data.isSocketError());
+                assert(!data.props._ret);
+                assert(data.props._error.msg == null);
+            });
+        });
+
+        test("input is string", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.SocketError("it is a socket error");
+                assert(data.isSocketError());
+                assert(!data.props._ret);
+                assert(data.props._error.msg == "it is a socket error");
+            });
+        });
+    });
+
+    describe("fromProps", () => {
+        test("props is undefined", () => {
+            assert.throws(() => AsyncRet.fromProps(), /fromProps must be passed an object./);
+        });
+
+        test("props is null", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.fromProps(null);
+                assert(data.props._ret == null);
+                assert(!data._msgId);
+            });
+        });
+
+        test("props is string", () => {
+            assert.throws(() => AsyncRet.fromProps("not an object"), /fromProps must be passed an object./);
+        });
+
+        test("props is object contains _ret", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.fromProps({_ret: 3});
+                assert(data.get() == 3);
+                assert(data.props._ret == 3);
+                assert(!data._msgId);
+            });
+        });
+
+        test("props is object contains _error", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.fromProps({
+                    _error: {
+                        type: "socket",
+                        msg: "An error message"
+                    }
+                });
+                assert(data.isSuccess() == false);
+                assert(data.errorMessage() == "An error message");
+                assert(data.errorType() == "socket");
+                assert(data.props._error.msg == "An error message");
+                assert(data.isSocketError());
+                assert(!data._msgId);
+            });
+        });
+
+        test("msgId is null", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.fromProps({_ret: 3}, null);
+                assert(data.get() == 3);
+                assert(data.props._ret == 3);
+                assert(data._msgId == null);
+            });
+        });
+
+        test("msgId is set", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.fromProps({_ret: 3}, 111222333);
+                assert(data.get() == 3);
+                assert(data.getProps()._ret == 3);
+                assert(data.msgId() == 111222333);
+                assert(data.props._ret == 3);
+                assert(data._msgId == 111222333);
+            });
+        });
+    });
+
+    describe("Timeout", () => {
+        test("input is undefined", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.Timeout();
+                assert(data.isTimeout());
+                assert(!data.props._ret);
+                assert(!data.props._error.msg);
+            });
+        });
+
+        test("input is null", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.Timeout(null);
+                assert(data.isTimeout());
+                assert(!data.props._ret);
+                assert(data.props._error.msg == null);
+            });
+        });
+
+        test("input is string", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.Timeout("it is a socket error");
+                assert(data.isTimeout());
+                assert(!data.props._ret);
+                assert(data.props._error.msg == "it is a socket error");
+            });
+        });
+    });
+
+    describe("Busy", () => {
+        test("input is undefined", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.Busy();
+                assert(!data.props._ret);
+                assert(!data.props._error.msg);
+                assert(data.props._error.type == "busy");
+            });
+        });
+
+        test("input is null", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.Busy(null);
+                assert(!data.props._ret);
+                assert(data.props._error.msg == null);
+                assert(data.props._error.type == "busy");
+            });
+        });
+
+        test("input is string", () => {
+            assert.doesNotThrow(() => {
+                const data = AsyncRet.Busy("it is a socket error");
+                assert(!data.props._ret);
+                assert(data.props._error.msg == "it is a socket error");
+                assert(data.props._error.type == "busy");
             });
         });
     });
