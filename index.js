c89283cee2cc5fea2a1358be7898c71f71fefdf9
Maicon Filippsen <filippsen@universe.ai>
2021-02-22 20:21:14 -0300

    Add index file exposing the public API

---
+++ index.js
@@ -0,0 +1,15 @@
+const AbstractAgent = require("./AbstractAgent");
+const AsyncRet = require("./AsyncRet");
+const Handshake = require("./Handshake");
+const Hub = require("./Hub");
+const Message = require("./Message");
+const MessageComm = require("./MessageComm");
+
+module.exports = {
+    AbstractAgent,
+    AsyncRet,
+    Handshake,
+    Hub,
+    Message,
+    MessageComm
+};
